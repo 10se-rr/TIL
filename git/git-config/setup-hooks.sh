36ab1b1e79e1831a85c789432b7eba6edf57f31b
이세중 <sejoong.biz@gmail.com>
2025-04-21 12:55:29 +0900

    docs: 실험중

--- git/git-config/setup-hooks.sh
+++ git/git-config/setup-hooks.sh
@@ -1,10 +1,13 @@
 # setup-hooks.sh
 
-# 1) 리포지토리 최상위 경로에서 이름 추출
+# 1) 변수 설정
 REPO_NAME=$(basename "$(git rev-parse --show-toplevel)")
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # 2) hooks 경로 설정
-git config core.hooksPath hooks
+git config core.hooksPath "$SCRIPT_DIR/setup-hooks.sh"
 
 # 3) 사용자 메시지 출력
 echo "✔️ ${REPO_NAME}에 Custom 훅이 적용되었습니다."
+
